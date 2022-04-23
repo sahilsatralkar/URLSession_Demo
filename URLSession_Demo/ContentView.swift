f8a04a5d5eda28423bd1c6f64321be73412d34b7
Sahil Satralkar <sahil.satralkar@gmail.com>
2022-04-23 14:34:02 +0530

    Initial Commit

---
+++ URLSession_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLSession_Demo
+//
+//  Created by Sahil Satralkar on 23/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLSession_Demo/URLSession_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_DemoApp.swift
+//  URLSession_Demo
+//
+//  Created by Sahil Satralkar on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
