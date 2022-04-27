7b87c9e3b79d7e34d8ed1d2ef0f131e9d165a970
Sahil Satralkar <sahil.satralkar@gmail.com>
2022-04-27 23:22:53 +0530

    commit

--- URLSession_Demo/Model.swift
+++ URLSession_Demo/Model.swift
@@ -13,4 +13,27 @@ struct Model : Codable, Identifiable {
     var email : String
     var gender : String
     var status : String
+    
+    func fetch() {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "gorest.co.in"
+        components.path = "/public/v2/users"
+        
+        
+        guard let url = components.url else {
+            preconditionFailure("Failed to construct URL")
+        }
+        
+//        let task = URLSession.shared.dataTask(with: url) {
+//            data, response, error in
+//
+//            DispatchQueue.main.async {
+//                if let data = data {
+//                    let model = [Model]
+//
+//                }
+//            }
+//        }
+    }
 }
